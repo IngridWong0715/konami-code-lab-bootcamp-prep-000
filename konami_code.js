43fecde174151cfd3565b22958d470cd4a1b2726
Ingrid Wong <yw340@georgetown.edu>
2017-12-17 21:29:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  var index = 0; 
-  
+  var index = 0;
+
   function trackKeyDown(e){
     var key = parseInt(e.detail || e.which);
     if (key === code[index]){
@@ -12,6 +12,8 @@ function init() {
         alert("YAY");
         index = 0;
       }
+    } else {
+      index = 0;
     }
   }
   document.body.addEventListene("keydown", trackKeyDown);
